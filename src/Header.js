5dbcd04607ec42cdb701d1f7434cc6c9fe4b34b7
Aaron Jordan <aaron@impactfulmedia.co>
2021-05-02 11:25:23 -0500

    updates to mobile styles and navigation

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,39 @@
+import {useState} from 'react';
 import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 import './Header.scss';
 
 export const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <header>
       <h1>Mazlin & Aaron</h1>
-      <nav className="nav-flex">
+      <div className={`mobile-controls ${isNavOpen?'open':'closed'}`} >
+        <div className="nav-padding" id="left"/>
+        <FontAwesomeIcon 
+          icon={faPlusCircle} 
+          id="navExpandIcon"
+          size="1x"
+          onClick={() => setIsNavOpen(x => !x)}
+        />
+        <FontAwesomeIcon 
+          icon={faPlusCircle} 
+          id="navExpandSlideIcon"
+          size="1x"
+          onClick={() => setIsNavOpen(x => !x)}
+        />
+        <div className="nav-padding" id="right"/>
+      </div>
+      <nav 
+        className={`nav-flex ${isNavOpen?'open':'closed'}`} 
+        onClick={() => isNavOpen && setIsNavOpen(false)}
+      >
         <div className="nav-padding" id="left"/>
         <Link to="/"><li>Home</li></Link>
         <Link to="/upcoming"><li>Upcoming</li></Link>
-        {/* <Link to="/l3"><li>RSVP</li></Link>
-        <Link to="/l4"><li>Registry</li></Link> */}
         <div className="nav-padding" id="right"/>
       </nav>
     </header>
