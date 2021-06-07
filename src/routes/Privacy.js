0a0dfb6dfd21a2f1481228109fe6878ce97a9046
Aaron Jordan <aaron@impactfulmedia.co>
2021-06-07 10:15:38 -0500

    added privacy policy for components in production build

---
+++ src/routes/Privacy.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import FlexBorderContainer from '../layout/FlexBorderContainer';
+
+const Privacy = () => {
+
+  // was converted from a markdown file...
+  return (
+    <main id="privacy-policy">
+      <FlexBorderContainer as='h1' id="privacy-policy" >Privacy Policy</FlexBorderContainer>
+      <h2 id="what-personal-information-do-we-collect-">What Personal Information Do We Collect?</h2>
+      <ul>
+      <li><strong>Account registration data</strong>: This is personal data provided by you personally through forms embedded in our site or through your choice to sign into our website through social media integration (Login with Facebook, Sign in with Google, etc.). Some of this data is required to create your account. Additional information is optional and only requested to improve your experience with our wedding and our website.</li>
+      </ul>
+      <h2 id="how-personal-information-is-used">How Personal Information is Used</h2>
+      <p>All data we collect is used to help us understand what to expect on our wedding day as well as ensure that our guests have an exceptional experience. Specifically, collected personal information does the following: </p>
+      <ul>
+      <li>Powers our RSVP records to help us prepare for our wedding day</li>
+      <li>May be used to contact you about our wedding</li>
+      <li>Helps us manage our guest list and set technical expectations for livestream attendees</li>
+      </ul>
+      <h2 id="how-we-protect-personal-information">How We Protect Personal Information</h2>
+      <p>All personal data collected is stored on a secured server designed to provide reasonable protection for your data. Security measures include firewalls and other, similar access control settings. To keep these measures effective, it is essential that you as a user maintain the security of your authentication method (your account at Facebook, Google, etc.).</p>
+      <h2 id="data-sharing">Data Sharing</h2>
+      <p>We do not share personal data collected on our website with any third-party services. We do accept personal data from third-party sources to enable logging into our website. </p>
+      <h2 id="use-of-cookies">Use of Cookies</h2>
+      <p>We use cookies on our website for a simple purposes:</p>
+      <ul>
+      <li><strong>Authentication</strong>: Cookies are used to establish and remember a user&#39;s identity on the application&#39;s servers. </li>
+      </ul>
+      <h2 id="control-over-personal-information">Control Over Personal Information</h2>
+      <p>If you would prefer that your personal data be changed or deleted by us, then please contact aaron@impactfulmedia.co explaining the details your request. We will be happy to make any change to or remove your data upon request.</p>
+      <h2 id="changes-to-this-policy">Changes to this Policy</h2>
+      <p>This policy may change and users who have logged into our application will receive notification by email if there are major changes. A revision date is included at the end of this section, indicating the last time that this policy was updated.</p>
+      <p id="privacyDateModified">June 7, 2021</p>
+    </main>
+  );
+  
+};
+
+export default Privacy;
